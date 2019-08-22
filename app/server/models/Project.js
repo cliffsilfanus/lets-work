6e481b3b1eb598119c6428716d41ab879bec6ffb
Clifford Silfanus <cliff.silfanus@gmail.com>
2019-08-21 20:25:45 -0400

    adjusted project and task schemas

--- app/server/models/Project.js
+++ app/server/models/Project.js
@@ -3,6 +3,13 @@ const mongoose = require("mongoose");
 
 const projectSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  headers: [
+    {
+      type: String,
+      enum: ["Date", "People", "Status", "Priority" /* , "Tag" */],
+      required: true
+    }
+  ],
   tasks: [{ type: mongoose.Schema.ObjectId, ref: "Task" }]
 });
 
--- app/server/models/Task.js
+++ app/server/models/Task.js
@@ -3,19 +3,24 @@ const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  components: [
-    {
-      component: {
-        type: String,
-        enum: ["Date", "People", "Status", "Priority", "Tags"],
-        required: true
-      },
-      date: { type: Date },
-      people: { type: mongoose.Schema.ObjectId, ref: "User" },
-      info: { type: String },
-      tag: { type: String }
-    }
-  ]
+  date: [{ type: Date }],
+  people: [{ type: String }],
+  status: { type: String },
+  priority: { type: String }
+  // components: [
+  //   {
+  //     component: {
+  //       type: String,
+  //       enum: ["Date", "People", "Status", "Priority", "Tags"],
+  //       required: true
+  //     },
+  //     date: { type: Date },
+  //     // people: { type: mongoose.Schema.ObjectId, ref: "User" },
+  //     users: [String],
+  //     info: { type: String },
+  //     tag: { type: String }
+  //   }
+  // ]
 });
 
 const Task = mongoose.model("Task", taskSchema);
